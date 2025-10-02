721acf30cd287f7e9ab2d6b697a8de6f0b20daff
Ahmedsamehm <a7medxd992@hotmail.com>
2025-10-02 17:02:58 +0300

    style: enhance heading styling in Hero component for improved visual impact

--- app/components/LandingPage/Hero.tsx
+++ app/components/LandingPage/Hero.tsx
@@ -1,7 +1,7 @@
 const Hero = ({ loadingResumes, resumes }: { loadingResumes: boolean; resumes: Resume[] }) => {
   return (
     <div className="page-heading py-16">
-      <h1>Track Your Applications & Resume Ratings</h1>
+      <h1 className="!text-3xl lg:!text-7xl !font-bold text-center">Track Your Applications & Resume Ratings</h1>
       {!loadingResumes && resumes?.length === 0 ? <h2>No resumes found. Upload your first resume to get feedback.</h2> : <h2>Review your submissions and check AI-powered feedback.</h2>}
     </div>
   );
